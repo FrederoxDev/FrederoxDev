c2a16928281ebbf70d665b605395885b50f359d2
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-05-20 13:52:25 +0100

    stuff

--- src/pages/BeyondRescue.tsx
+++ src/pages/BeyondRescue.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import MarketingImg from "../assets/portfolio/Image-3.png";
 
 export function BeyondRescue() {
     return (
-        <>
+        <div className="min-h-screen overscroll-none overflow-y-auto">
             <div className="bg-[url('./assets/portfolio/corkboard.png')] w-full">
-                <p className="text-black text-3xl text-center">
+                <p className="text-white text-6xl text-center pt-10 font-bold font-serif">
                     Beyond Rescue
                 </p>
 
@@ -12,25 +13,38 @@ export function BeyondRescue() {
                     <img src={MarketingImg} alt="Marketing" />
                 </div>
             </div>
-            <div className="bg-blue-500 ">
+            <div className="inset-0 -z-10 bg-[#275E79]
+        bg-[linear-gradient(to_right,#98C9E120_1px,transparent_1px),linear-gradient(to_bottom,#98C9E120_1px,transparent_1px)]
+        bg-[size:80px_80px]">
                 <div className="flex justify-center">
-                    <iframe
-                        width="560"
-                        height="315"
-                        src="https://www.youtube-nocookie.com/embed/MNRdehuVPGQ?si=2wEBVltqD3F_JK05"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerPolicy="strict-origin-when-cross-origin"
-                        allowFullScreen
-                        className="mt-10  mb-10"
-                    >
-                    </iframe>
                     <div>
-                        <p>Downloads</p>
+                        <p className="text-center text-white text-4xl font-bold font-serif mt-10">
+                            Gameplay
+                        </p>
+                        <iframe
+                            width="840"
+                            height="472.5"
+                            src="https://www.youtube-nocookie.com/embed/MNRdehuVPGQ?si=2wEBVltqD3F_JK05"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                            className="mt-10  mb-10"
+                        >
+                        </iframe>
                     </div>
                 </div>
+                <div className="flex justify-center">
+                    <Link 
+                        className="text-center text-blue-500 text-4xl font-bold font-serif mt-10 bg-white p-4 cursor-pointer"
+                        to="https://frederox.itch.io/beyond-rescue"
+                    >
+                        Play on Itch.io
+                    </Link>
+                </div>
+                <div className="h-20 w-full"></div>
             </div>
-        </>
+        </div>
     );
 }
