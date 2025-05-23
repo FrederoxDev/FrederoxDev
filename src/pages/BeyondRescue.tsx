0f205732171144723037d950e827eed34a5d2364
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-05-23 22:52:10 +0100

    uopd

--- src/pages/BeyondRescue.tsx
+++ src/pages/BeyondRescue.tsx
@@ -24,7 +24,7 @@ export function BeyondRescue() {
                         <iframe
                             width="840"
                             height="472.5"
-                            src="https://www.youtube.com/embed/bl6S4Koo_d4?si=ekpGjbVr5EqgMjhc"
+                            src="https://www.youtube.com/embed/2FECZHilLUE"
                             title="YouTube video player"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
