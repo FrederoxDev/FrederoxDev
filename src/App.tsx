610982938e6a3d527b30d255e764365bba0fe077
FrederoxDev <blakefreddie96@gmail.com>
2026-01-11 21:10:19 +0000

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 
 function App() {
